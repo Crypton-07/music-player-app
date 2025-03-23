9f3bc6bbd2955c7ec1d88871b9d83e5ed5a07f4a
vishesh <visheshprajapati786@gmail.com>
2025-03-23 16:39:50 +0530

    Re-implemented changes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -26,9 +26,9 @@ const SideBar = () => {
     >
       <div className="sidebar-header">
         <img
+          className="logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Spotify_logo_with_text.svg/2560px-Spotify_logo_with_text.svg.png"
           alt="logo"
-          width={100}
         />
         {showSidebar && (
           <button
@@ -54,7 +54,7 @@ const SideBar = () => {
         </Nav.Link>
 
         <Nav.Link
-          className={activeTab === "recent" ? "active" : ""}
+          className={activeTab === "favorites" ? "active" : ""}
           onClick={() => {
             setActiveTab("favorites");
             setShowSidebar(false);
